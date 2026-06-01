b420731ba8b88ea67f2ceaf808c4c4975bff93f9
architraj99 <rajarchit99@gmail.com>
2026-06-01 23:52:46 +0530

    Made basic game layout

---
+++ script.js
@@ -0,0 +1,9 @@
+const gold = document.getElementById("gold");
+const lives = document.getElementById("lives");
+const wave = document.getElementById("wave");
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", () => {
+    
+    startBtn.textContent = "Game Started";
+});
